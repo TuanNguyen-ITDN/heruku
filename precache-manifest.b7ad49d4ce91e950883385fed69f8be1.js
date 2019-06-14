c07ac76e9f875d3063ada7c2ba883d762caeef89
unknown <Administrator@asvnpnpc19.pn.com.vn>
2019-06-14 10:54:32 +0700

    ok

---
+++ precache-manifest.b7ad49d4ce91e950883385fed69f8be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b80da098769aedabd38515efef3c27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d5a1c50c21865c13cc",
+    "url": "/static/css/main.b57b2749.chunk.css"
+  },
+  {
+    "revision": "486f94e85f4438b6ee98",
+    "url": "/static/js/2.10eb33de.chunk.js"
+  },
+  {
+    "revision": "16d5a1c50c21865c13cc",
+    "url": "/static/js/main.2b8de125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
